28a3666f77d0e255614a8a734f24d1c78045be5a
sgsettle <s.settle41@gmail.com>
2020-08-27 10:30:44 -0400

    beginning of api built out classes

---
+++ GeneralStoreAPI/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
---
+++ GeneralStoreAPI/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public int NumberInStock { get; set; }
+        public bool IsInStock
+        {
+            get
+            {
+                
+            }
+        }
+    }
+}
